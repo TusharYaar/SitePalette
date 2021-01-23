da254a5608c0db5c354aebb78eca7b864fe852e9
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2021-01-23 23:13:13 +0530

    Basic Add and remove class working

--- assests/indexScript.js
+++ assests/indexScript.js
@@ -1,1 +1,31 @@
-// alert("connected");
+document.addEventListener("DOMContentLoaded", function () {
+  // function to add active class to the interaction buttons
+  document.querySelectorAll(".site-interaction").forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.stopPropagation();
+      removeClass(".site-interaction", "active");
+      this.classList.add("active");
+      setTimeout(() => {
+        this.children[0].classList.add("hide");
+      }, 100);
+    });
+  });
+
+  document.querySelectorAll(".close-icon").forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.stopPropagation();
+      removeClass(`#${this.parentElement.parentElement.id}`, "active");
+      setTimeout(function () {
+        removeClass(`.site-interaction > img`, "hide");
+      }, 250);
+      //   console.log(this.parentElement.id);
+    });
+  });
+});
+// Function to remove class from all the elements with same selector and the class
+function removeClass(string, clas) {
+  document.querySelectorAll(string).forEach((element) => {
+    element.classList.remove(clas);
+    console.log(element);
+  });
+}
