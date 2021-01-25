334683f971aa5c0faf0c9687de55789d27075136
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2021-01-26 02:32:08 +0530

    Major Update...Lock Working  Template Switching working

--- assests/indexScript.js
+++ assests/indexScript.js
@@ -5,16 +5,17 @@ document.addEventListener("DOMContentLoaded", function () {
   }
   checkWidth();
   loadSiteTemplateList();
+  loadColorSelectList();
+  // loadTemplateScripts();
   // function to add active class to the interaction buttons
   document.querySelectorAll(".site-interaction").forEach(function (element) {
     element.addEventListener("click", function (e) {
       e.stopPropagation();
       removeClass(".site-interaction", "active");
+      this.classList.add("active");
       setTimeout(function () {
         removeClass(`.site-interaction > img`, "hide");
       }, 90);
-
-      this.classList.add("active");
       setTimeout(() => {
         this.children[0].classList.add("hide");
       }, 100);
@@ -24,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".close-icon").forEach(function (element) {
     element.addEventListener("click", function (e) {
       e.stopPropagation();
+      e.preventDefault();
       removeClass(`#${this.parentElement.parentElement.id}`, "active");
       setTimeout(function () {
         removeClass(`.site-interaction > img`, "hide");
@@ -33,9 +35,24 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".copy-icon").forEach(function (element) {
     element.addEventListener("click", function (e) {
       e.stopPropagation();
+      e.preventDefault();
       var id = this.parentElement.attributes["for"].value;
       document.getElementById(id).select();
       document.execCommand("copy");
+    });
+  });
+  document.querySelectorAll(".lock-icon").forEach(function (element) {
+    element.addEventListener("click", function (e) {
+      e.stopPropagation();
+      e.preventDefault();
+      var id = this.parentElement.attributes["for"].value;
+      this.classList.toggle("locked");
+      console.log(id);
+      arr = currentColorData.map((e) => {
+        if (id === e.id) e.locked = !e.locked;
+        return e;
+      });
+      console.log(arr);
     });
   });
 });
@@ -50,24 +67,56 @@ function loadSiteTemplateList() {
   var element;
   ul.innerHTML = "";
   templateData.forEach((item) => {
-    console.log(item);
-    element = `<li><label for="${item.id}">${item.name}</label><input type="radio" name="change-template-option" id="${item.id}" onclick="load_template('${item.functionCallName}')" /></li>`;
-    console.log(element);
+    element = `<li><label for="${item.id}">${item.name}</label><input type="radio" name="change-template-option" id="${item.id}" onclick="loadTemplate('${item.functionCallName}')" /></li>`;
     ul.insertAdjacentHTML("beforeend", element);
   });
 }
-function load_template(page) {
-  console.log(page);
-  document.getElementById("site-template").innerHTML = `<object type="text/html" data="./assests/templates/html/${page}.html" ></object>`;
+// function loadTemplateScripts() {
+//   templateData.forEach(function (page) {
+//     var src = `assests/templates/html/${page.functionCallName}.js`;
+//     document.querySelector("body").insertAdjacentHTML("beforeend", `<script src="${src}"></script>`);
+//     document.querySelector("#lastElement").insertAdjacentHTML("afterend", `<link rel="stylesheet" href="assests/templates/styles/${page.functionCallName}.css" >`);
+//   });
+// }
+
+function loadTemplate(page) {
+  var xhr = new XMLHttpRequest();
+  var url = `assests/templates/html/${page}.html`;
+  xhr.open("GET", url);
+  xhr.send();
+  document.querySelector("#site-template-css").setAttribute("href", `assests/templates/styles/${page}.css`);
+  xhr.onload = function () {
+    document.querySelector("#site-template").innerHTML = this.responseText;
+  };
 }
+// function loadTemplate(page) {
+//   var src = `assests/templates/html/${page}.js`;
+//   var css = `assests/templates/css/${page}.css`;
+//   document.getElementById("site-template-js").setAttribute("src", src);
+//   document.getElementById("site-template-css").setAttribute("link", css);
+//   renderDocument();
+// }
 
 // Function to fetch a random color palette from the array and set the details of the preview box and the input field
 function getRandomColorData() {
   var value = Math.floor(Math.random() * colorData.length);
   colorData[value].forEach(function (color, index) {
     if (index < 5) {
-      document.querySelector(`#color${index + 1}`).value = `#${color}`;
-      document.querySelector(`#color${index + 1}-parent > label> span`).style.backgroundColor = `#${color}`;
+      if (!currentColorData[index].locked) {
+        currentColorData[index].color = color;
+        document.querySelector(`#color${index + 1}`).value = `#${color}`;
+        document.querySelector(`#color${index + 1}-parent > label> span`).style.backgroundColor = `#${color}`;
+      }
     }
+  });
+}
+function loadColorSelectList() {
+  var ul = document.getElementById("change-color-list");
+  var element;
+  ul.innerHTML = "";
+  currentColorData.forEach(function (data) {
+    console.log(data);
+    element = `<li id="${data.id}-parent"><label for="${data.id}">${data.name} <span class="color-preview"></span>${copyIcon}</label><input type="text" id="${data.id}" /><label for="${data.id}">${lockIcon}</label></li>`;
+    ul.insertAdjacentHTML("beforeend", element);
   });
 }
--- assests/variableData.js
+++ assests/variableData.js
@@ -1,8 +1,21 @@
 templateData = [
-  { name: "Facecopy", functionCallName: "facecopy", id: "home" },
-  { name: "PreviewDocument", functionCallName: "previewDocument", id: "preview" },
+  {
+    name: "Facecopy",
+    functionCallName: "facecopy",
+    id: "home",
+  },
+  {
+    name: "Preview Document",
+    functionCallName: "previewDocument",
+    id: "preview",
+  },
 ];
-
-// `
-// <label for="Home">Home</label>
-// <input type="radio" name="Home" id="Home" onclick="load_home('facecopy')" />`;
+const lockIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="White" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-lock lock-icon"><rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg>`;
+const copyIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-copy copy-icon"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>`;
+currentColorData = [
+  { name: "Text", id: "color1", color: "black", locked: false },
+  { name: "background", id: "color2", color: "black", locked: false },
+  { name: "body", id: "color3", color: "black", locked: false },
+  { name: "button", id: "color4", color: "black", locked: false },
+  { name: "Something", id: "color5", color: "black", locked: false },
+];
