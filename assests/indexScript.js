1f92a37532d1c836b6a92b24984d5b85a071aca0
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2021-03-10 22:32:58 +0530

    shopping site update

--- assests/indexScript.js
+++ assests/indexScript.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
   checkWidth(); // Calls the function to check width of the screen and if smaller than 558 pixels, show the overlay
   loadSiteTemplateList(); // Loads the site template list
   getSavedColors(); // Calls the get saved color to get saved colors from localStorage
-  loadTemplate("landingPage", "Editorial Site");
+  loadTemplate("landingPage", "Landing Page");
   fillAdvanceSelect();
 
   // showSiteMessage("New to the site!!! Start by taking a basic tour", true, 1500);
